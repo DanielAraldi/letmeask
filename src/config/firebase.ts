1eaff831e2b358f1ef927590a649aeca40ff097a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-01-22 16:15:04 -0300

    feat: added the firebase configurations for auth and realtime database

---
+++ src/config/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp, FirebaseOptions } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+import { env } from './env';
+
+const firebaseOptions: FirebaseOptions = {
+  apiKey: env.apiKey,
+  authDomain: env.authDomain,
+  databaseURL: env.databaseURL,
+  projectId: env.projectId,
+  storageBucket: env.storageBucket,
+  messagingSenderId: env.messagingSenderId,
+  appId: env.appId,
+};
+
+const app = initializeApp(firebaseOptions);
+
+export const auth = getAuth(app);
+export const database = getDatabase(app);
