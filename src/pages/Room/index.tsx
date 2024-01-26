7aa72ed7f1521b77f108d0bf7213019e0db8cea3
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-01-26 17:39:45 -0300

    feat: created `Room` page with your structure and styles (that isn't complete still)

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,3 +1,40 @@
+import '../../styles/room.scss';
+
+import { useParams } from 'react-router-dom';
+
+import { Button, RoomCode } from '../../components';
+import { LOGO } from '../../config';
+
 export function Room() {
-  return <h1>Hello World!</h1>;
+  const params = useParams();
+
+  return (
+    <div id='page-room'>
+      <header>
+        <div className='content'>
+          <img src={LOGO} alt='Letmeask' />
+          <RoomCode code={params.id!} />
+        </div>
+      </header>
+
+      <main>
+        <div className='room-title'>
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder='O que você quer perguntar?' />
+
+          <div className='form-footer'>
+            <span>
+              Para enviar uma pergunta, <button>faça seu login.</button>
+            </span>
+
+            <Button type='submit'>Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
