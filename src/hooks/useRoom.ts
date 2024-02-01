cc94c162105fb5920ecb0976b189241a608ce888
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-02-01 17:34:11 -0300

    fix: add sort questions to send highlight questions to the list top and answered questions to the list bottom

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -16,22 +16,37 @@ export function useRoom(roomId: string): RoomHookProps {
   const [title, setTitle] = useState<string>('');
   const [questions, setQuestions] = useState<ParsedQuestionProps[]>([]);
 
+  function sortQuestion(
+    question: ParsedQuestionProps,
+    questionCompare: ParsedQuestionProps,
+  ): number {
+    if (question.isHighlighted && !questionCompare.isHighlighted) return -1;
+    else if (!question.isHighlighted && questionCompare.isHighlighted) return 1;
+
+    if (question.isAnswered && !questionCompare.isAnswered) return 1;
+    else if (!question.isAnswered && questionCompare.isAnswered) return -1;
+
+    return 0;
+  }
+
   useEffect(() => {
     const roomRef = databaseRef(database, `rooms/${roomId}`);
 
     const unsubscribe = onValueInDatabase(roomRef, room => {
       const roomValue: FirebaseRoomProps = room.val();
       const questions: FirebaseQuestionsProps = roomValue.questions ?? {};
-      const parsedQuestions = Object.entries(questions).map(
-        ([key, question]): ParsedQuestionProps => ({
-          id: key,
-          likeCount: Object.values(question.likes ?? {}).length,
-          likeId: Object.entries(question.likes ?? {}).find(
-            ([, like]) => like.authorId === user?.id,
-          )?.[0],
-          ...question,
-        }),
-      );
+      const parsedQuestions = Object.entries(questions)
+        .map(
+          ([key, question]): ParsedQuestionProps => ({
+            id: key,
+            likeCount: Object.values(question.likes ?? {}).length,
+            likeId: Object.entries(question.likes ?? {}).find(
+              ([, like]) => like.authorId === user?.id,
+            )?.[0],
+            ...question,
+          }),
+        )
+        .sort(sortQuestion);
 
       setIsClosed(!!roomValue.closedAt);
       setTitle(roomValue.title);
